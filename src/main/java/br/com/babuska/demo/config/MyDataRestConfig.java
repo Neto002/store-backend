3494807b82dd4944b7fdce9175012abeb96ef328
Neto002 <antoniogfneto92@hotmail.com>
2024-01-31 17:09:43 -0300

    Adding keywork searching

--- src/main/java/br/com/babuska/demo/config/MyDataRestConfig.java
+++ src/main/java/br/com/babuska/demo/config/MyDataRestConfig.java
@@ -2,28 +2,55 @@ package br.com.babuska.demo.config;
 
 import br.com.babuska.demo.model.Product;
 import br.com.babuska.demo.model.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+        // Enable readonly actions and methods with CORS for each entity class.
+        for (Class<?> entityClass : new Class[]{Product.class, ProductCategory.class}) {
+            config.getExposureConfiguration()
+                    .forDomainType(entityClass)
+                    .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                    .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+        }
 
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class<?>> entityClasses = new ArrayList<>();
+
+        for (EntityType<?> tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 
 }
